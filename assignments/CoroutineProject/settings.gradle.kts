2ff150ea5117d9af09d1338d7591b913fefc745d
anahshal7 <143558945+anahshal7@users.noreply.github.com>
2023-11-30 14:11:13 -0500

    update

---
+++ assignments/CoroutineProject/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "coroutine_project"
+include(":app")
+ 
